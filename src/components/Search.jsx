a5b8c8c1a384ac5bf6f7281a2ac608c52024bc6c
YURIKA PRAJAPATI <prajapatiyurika@gmail.com>
2025-04-25 01:14:25 +0545

    initial, Search

---
+++ src/components/Search.jsx
@@ -0,0 +1,66 @@
+import {useState} from 'react'
+import styled from 'styled-components'
+import { FaSearch } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom';
+
+function Search() {
+
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate("/searched/" + input);
+  }
+
+  return (
+    <CenterWrapper>
+      <FormStyle onSubmit={submitHandler}>
+      <FaSearch></FaSearch>
+      <input type="text" 
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+   
+    </FormStyle>
+    </CenterWrapper>
+  )
+}
+
+const CenterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20vh; /* Full screen height */
+  position: static;
+
+  
+`;
+
+const FormStyle = styled.form `
+  margin: 0rem 20rem;
+  position: relative;
+  width: 100%;
+
+  input{
+    border: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    font-size: 1.5rem;
+    color: white;
+    padding: 1rem 3rem;
+    border: none;
+    border-radius: 1rem;
+    width: 100%;
+    outline: none;
+  }
+
+  svg{
+    position: absolute;
+    top:50%;
+    left: 0%;
+    transform: translate(100%, -50%);
+    color: white;
+  }
+`
+
+export default Search
